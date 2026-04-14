a2217ed4f554393e2c290a18989eddc58d473aea
M Hamza Aftab <mhamzaaftab1166@gmail.com>
2026-04-14 23:41:47 +0400

    cconnected frontend with backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,6 +8,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from TypeScript Express!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from TypeScript Express!" });
+});
+
 // 🔹 PORT
 const PORT: number = Number(process.env.PORT) || 3000;
 
