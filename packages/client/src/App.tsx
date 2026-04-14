5ef644c41299c986bb40ef8c2c1da426bb718b56
M Hamza Aftab <mhamzaaftab1166@gmail.com>
2026-04-15 00:28:52 +0400

    setup shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -13,8 +14,9 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <p className="font-bold p-4 text-3xl"> {message}</p>
+    <div className="p-4">
+      <p className="font-bold text-3xl"> {message}</p>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
