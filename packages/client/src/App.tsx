142dbdf85820aafb4e9ceafd9222fe2ba3fcbf77
M Hamza Aftab <mhamzaaftab1166@gmail.com>
2026-04-15 00:31:52 +0400

    fix bug

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="p-4">
-      <p className="font-bold text-3xl"> {message}</p>
+      <p className="font-bold  text-3xl"> {message}</p>
       <Button variant="outline">Button</Button>
     </div>
   );
